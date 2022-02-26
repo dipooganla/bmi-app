aa62df7efcadd265cb876db6893e9bd2126043ec
dipooganla <doganla1992@gmail.com>
2022-02-26 13:53:49 +0000

    changed html and api endpoint in server

--- calculator.js
+++ calculator.js
@@ -2,20 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser")
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
-app.post("/bmicalculator", function(req, res) {
+app.post("/", function(req, res) {
 
-    var weight = Number(req.body.num1);
-    var height = Number(req.body.num2);
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
 
     var result = weight / (height * height);
 
-    res.send("Your BMI is " + result)
+    res.send('Your BMI is ' + result)
 });
 
 app.listen(3000, function() {
